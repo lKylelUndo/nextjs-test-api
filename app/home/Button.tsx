7605ec0e136111752e7b61d6f64cd8a6b8f75cbd
Kyle Ando <ando.kyle44@gmail.com>
2025-08-31 18:09:21 +0800

    fixingg useeffect

--- app/home/Button.tsx
+++ app/home/Button.tsx
@@ -1,7 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 
 function Button() {
+  useEffect(() => {
+    async function getTodos() {
+      try {
+        const res = await fetch(
+          "https://nextjs-test-api-alpha.vercel.app/api/todos"
+        );
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getTodos();
+  }, []);
   return (
     <div>
       <button onClick={() => alert("hello")}>click me</button>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,37 +1,7 @@
-"use client";
 import React, { useEffect } from "react";
 import Button from "./Button";
 
-// export async function getTodos() {
-//   const res = await fetch("https://dummyjson.com/todos");
-//   const data = await res.json();
-//   return data;
-// }
-
 async function page() {
-  const res = await fetch(
-    "https://nextjs-test-api-alpha.vercel.app/api/todos",
-    {
-      method: "GET",
-    }
-  );
-  console.log(await res.json());
-
-  useEffect(() => {
-    async function getTodos() {
-      try {
-        const res = await fetch(
-          "https://nextjs-test-api-alpha.vercel.app/api/todos"
-        );
-        const data = await res.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    getTodos();
-  }, []);
   return (
     <div>
       <Button />
